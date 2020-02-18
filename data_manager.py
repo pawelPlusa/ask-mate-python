9bd5e6e558e98837c66b2de8837f0f6c1ba7a9b1
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-02-18 15:15:31 +0100

    before getting util.py

--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,7 @@
 import connection
 
 QUESTION_FILE_PATH = "sample_data/question.csv"
-ANSWERS_FILE_PATH = "sample_data/question.csv"
+ANSWERS_FILE_PATH = "sample_data/answer.csv"
 
 QUESTIONS = connection.open_file(QUESTION_FILE_PATH)
 ANSWERS = connection.open_file(ANSWERS_FILE_PATH)
-
