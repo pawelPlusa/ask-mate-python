07babce5d3009acc24e349783ae60edbc0590fec
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-02-18 14:49:51 +0100

    minor changes to server

--- server.py
+++ server.py
@@ -1,12 +1,31 @@
+'''
+Questions format:
+[{id: int, submission_time: time_stamp (int), view_number: int, vote_number: int, title: str, message: str, image: url(str)},
+...
+]
+
+Answers format:
+[{id: int,submission_time: time_stamp (int), vote_number: int, question_id: int, message: str, image: url(str)},
+...
+]
+'''
+
+
 from flask import Flask, render_template, request, redirect
 import data_manager
-import csv
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def start():
     return render_template("index.html")
+
+
+@app.route("/question/<question>?<sorted=date>")
+def show_answers(question_id, sort='date', reverse=True):
+
+    return render_template('list_comments.html', question)
 
 
 if __name__ == "__main__":
