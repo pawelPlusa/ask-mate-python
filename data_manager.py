96b1a89ec1b89a0eef3d54784d8e3f2aa469f1bf
corumov <ppplusa@gmail.com>
2020-02-18 13:47:15 +0100

    fundamental version of data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,7 +1,8 @@
 import connection
 
 QUESTION_FILE_PATH = "sample_data/question.csv"
-ANSWER_FILE_PATH = "sample_data/question.csv"
+ANSWERS_FILE_PATH = "sample_data/question.csv"
 
-QUESTIONS_DICT = connection.open_file(QUESTION_FILE_PATH)
-print(QUESTIONS_DICT)
+QUESTIONS = connection.open_file(QUESTION_FILE_PATH)
+ANSWERS = connection.open_file(ANSWERS_FILE_PATH)
+
