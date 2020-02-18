00ba6bb09c4aa025a27d16af6acabb55fd87ad63
corumov <ppplusa@gmail.com>
2020-02-18 14:59:57 +0100

    added get answer to question function in util

---
+++ util.py
@@ -0,0 +1,4 @@
+from data_manager import QUESTIONS
+
+def find_answers_by_question(question_id, answers_file) -> list :
+    return [single_answer for single_answer in answers_file if int(single_answer["question_id"]) == int(question_id)]
