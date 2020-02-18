e070e339e21fa69973c801e58191a63676c07fcd
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-02-18 15:36:47 +0100

    Function find_answers_by_question in util.py does not wokrk

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 from data_manager import QUESTIONS
 
+# TODO: This function does not work
+
 def find_answers_by_question(question_id, answers_file) -> list :
     return [single_answer for single_answer in answers_file if int(single_answer["question_id"]) == int(question_id)]
