e1d3040d5d83a024b2f68b7bf876a9ab696777c5
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-02-18 15:54:48 +0100

    Function find_answers_by_question works
    Bug fixed, there was wrong key in sigle_answer.
    Also int removed, it is easier to compare strings,
    than converting to strings to integers and comparing.

--- util.py
+++ util.py
@@ -1,6 +1,14 @@
-from data_manager import QUESTIONS
+'''
+This module defines additional functions.
+'''
 
-# TODO: This function does not work
+def find_answers_by_question(question_id: str, answers_file: list) -> list:
+    """
+    Finds answers(list) for selected question_id
+    param: question_id: str
+    param: answers_file: list
+    returns list of answers: list
+    if there is no question with given id, returns empty list
+    """
 
-def find_answers_by_question(question_id, answers_file) -> list :
-    return [single_answer for single_answer in answers_file if int(single_answer["question_id"]) == int(question_id)]
+    return [single_answer for single_answer in answers_file if single_answer["id"] == question_id]
