a392371c7162d681f1e4f802c25ac4c172089185
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-02-19 21:49:42 +0100

    Add function find_next_id to util.py
    Function finds next FREE id,
    so if there are id: 0, 1, 2 it gives 3 as new id
    but if there is a gap like 0, 2, 3 it gives 1 (first free number)

--- util.py
+++ util.py
@@ -2,6 +2,7 @@
 This module defines additional functions.
 '''
 
+
 def find_answers_by_question(question_id: str, answers_file: list) -> list:
     """
     Finds answers(list) for selected question_id
@@ -12,3 +13,18 @@ def find_answers_by_question(question_id: str, answers_file: list) -> list:
     """
 
     return [single_answer for single_answer in answers_file if single_answer["question_id"] == question_id]
+
+
+def find_next_id(contents_list: list) -> str:
+    """
+    Finds new id
+    param: contents_list: list of dicts
+    returns id: str
+    """
+
+    number = 0
+    while number < len(contents_list):
+        if contents_list[number]['id'] != str(number):
+            return str(number)
+        number += 1
+    return str(number)
