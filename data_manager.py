5c144641b3cb4f1d6694b2df8700358e8d889f49
Lukasz Mielczarek <lukasz.mielczarek1989@gmail.com>
2020-02-19 13:23:10 +0100

    i justwanna pull2

--- data_manager.py
+++ data_manager.py
@@ -6,3 +6,6 @@ ANSWERS_FILE_PATH = "sample_data/question.csv"
 QUESTIONS = connection.open_file(QUESTION_FILE_PATH)
 ANSWERS = connection.open_file(ANSWERS_FILE_PATH)
 
+
+
+
--- server.py
+++ server.py
@@ -18,6 +18,16 @@ def show_questions(sorted_by,direction):
     sorted_questions = sorted(QUESTIONS, key= lambda i: i[sorted_by], reverse= direction)
     return render_template("list.html", sorted_questions=sorted_questions)
 
+@app.route("/question/<question_id>/edit", methods = ["GET","POST"])
+def edit_question(question_id):
+    question_data = {}
+    if request.method == "POST":
+        question_data['question_title'] = request.form['question_title']
+        question_data['question'] = request.form['question']
+    print(question_data)
+
+
+
 
 if __name__ == "__main__":
     app.run(
