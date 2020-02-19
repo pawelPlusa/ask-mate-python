a4cd4058a0585ad2bdfc5066e856d2780db25097
Lukasz Mielczarek <lukasz.mielczarek1989@gmail.com>
2020-02-19 09:32:29 +0100

    show list q, links in list.html, display in list.html

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, request, redirect
-import csv
+import connection
+from data_manager import QUESTIONS
 
 app = Flask(__name__)
 
 @app.route("/")
 def start():
     return render_template("index.html")
+
+@app.route("/list")
+def show_questions_list():
+    sorted_questions = sorted(QUESTIONS, key= lambda i: i['submission_time'], reverse=1)
+    return render_template("list.html", sorted_questions = sorted_questions )
+
+@app.route("/list/<sorted_by>/<int:direction>")
+def show_questions(sorted_by,direction):
+    sorted_questions = sorted(QUESTIONS, key= lambda i: i[sorted_by], reverse= direction)
+    return render_template("list.html", sorted_questions=sorted_questions)
 
 
 if __name__ == "__main__":
