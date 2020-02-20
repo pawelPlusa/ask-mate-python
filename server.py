facd6e38bc76ed5e17defa8bb8a2cf8e1526273d
corumov <ppplusa@gmail.com>
2020-02-20 17:32:43 +0100

    added question vote logic to server, slight changes in css

--- server.py
+++ server.py
@@ -23,11 +23,14 @@ app = Flask(__name__)
 @app.route("/")
 def start():
     sorted_questions = sorted(data_manager.QUESTIONS, key= lambda i: i['submission_time'], reverse=1)
-    return render_template("index.html", sorted_questions=util.change_time_format(sorted_questions))
+
+    return render_template("index.html", sorted_questions=util.change_time_format(sorted_questions),
+                           headers=list(sorted_questions[0].keys())[1:])
 
 # Łukasz
 
 @app.route("/list")
+
 def show_questions_list():
     sorted_questions = sorted(data_manager.QUESTIONS, key= lambda i: i['submission_time'], reverse=1)
     return render_template("list.html", sorted_questions=util.change_time_format(sorted_questions))
@@ -42,11 +45,23 @@ def show_questions(sorted_by,direction):
 
 @app.route("/questions/<question_id>")
 @app.route("/questions/<question_id>/<sorted_by>/<int:direction>")
-def show_answers(question_id, sorted_by=None, direction=0):
+@app.route("/questions/<question_id>/<vote>")
+
+def show_answers(question_id, sorted_by=None, direction=0, vote=None):
     question_id = data_manager.QUESTIONS[int(question_id)]['id']
     question_title = data_manager.QUESTIONS[int(question_id)]['title']
     question_message = data_manager.QUESTIONS[int(question_id)]['message']
     answers = util.find_answers_by_question(question_id, data_manager.ANSWERS)
+    if vote:
+        votes_no = int(data_manager.QUESTIONS[int(question_id)]["vote_number"])
+        if vote == "vote_down" and votes_no > 0:
+            votes_no -= 1
+        elif vote == "vote_up":
+            votes_no += 1
+        data_manager.QUESTIONS[int(question_id)]["vote_number"] = votes_no
+        connection.save_file(data_manager.QUESTIONS, data_manager.QUESTION_FILE_PATH)
+        return redirect("/list", code=303)
+    return "problem"
 
     if sorted_by:
         answers.sort(key=lambda item: item[sorted_by], reverse=direction)
