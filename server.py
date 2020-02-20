2d73b70da866dc9ba7030db3a3694fac6cbde87e
tomaszleszczynski1980 <lin.fowen@gmail.com>
2020-02-20 09:19:35 +0100

    Function add_answer maintained

--- server.py
+++ server.py
@@ -42,11 +42,14 @@ def show_questions(sorted_by,direction):
 
 @app.route("/questions/<question_id>")
 def show_answers(question_id):
+    question_id = data_manager.QUESTIONS[int(question_id)]['id']
     question_title = data_manager.QUESTIONS[int(question_id)]['title']
     question_message = data_manager.QUESTIONS[int(question_id)]['message']
     answers = util.find_answers_by_question(question_id, data_manager.ANSWERS)
+    print(answers)
 
-    return render_template('questions.html', question_title=question_title,
+    return render_template('questions.html',
+                           question_id=question_id, question_title=question_title,
                            question_message=question_message, answers=answers)
 
 
